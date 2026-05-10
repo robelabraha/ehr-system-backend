fc7b0044166f772e8dc1383c25323e5700c94293
Robel T Abraha <abraharobel10@gmail.com>
2026-05-11 05:19:24 +0530

    Create Patient.java

---
+++ src/main/java/com/ehr/patient/Patient.java
@@ -0,0 +1,24 @@
+package com.ehr.patient;
+
+public class Patient {
+    private Long id;
+    private String name;
+    private String email;
+
+    public Patient() {}
+
+    public Patient(Long id, String name, String email) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public Long getId() { return id; }
+    public void setId(Long id) { this.id = id; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getEmail() { return email; }
+    public void setEmail(String email) { this.email = email; }
+}
