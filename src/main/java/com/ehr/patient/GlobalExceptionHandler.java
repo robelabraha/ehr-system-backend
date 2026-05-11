f781452e6f971e0606739b19c669f5dceada376f
Robel T Abraha <abraharobel10@gmail.com>
2026-05-11 05:47:04 +0530

    Create GlobalExceptionHandler.java

---
+++ src/main/java/com/ehr/patient/GlobalExceptionHandler.java
@@ -0,0 +1,14 @@
+package com.ehr.patient;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    public ResponseEntity<String> handleRuntimeException(RuntimeException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+}
